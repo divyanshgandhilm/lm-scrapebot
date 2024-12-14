9f9a496906a76ff6c3aa22603da6f8abaa5f1e7f
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-12-14 07:15:41 +0530

    chore: missing directory for index

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
-import { main } from './index.js';
+import { main } from '../index.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
